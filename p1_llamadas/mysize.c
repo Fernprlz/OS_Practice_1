3a4fa119b8172700f5aac42d2fa173d2ac620844
Fernpelz <fern.perloz@gmail.com>
2019-03-02 00:30:34 +0100

    mysize error msg

--- p1_llamadas/mysize.c
+++ p1_llamadas/mysize.c
@@ -5,6 +5,8 @@
 #include <dirent.h>
 #include <unistd.h>
 
+#include <errno.h>
+
 /*
 * - Obtain the current directory with getcwd();
 * - Read the entries of the directory using readdir
@@ -16,24 +18,30 @@
 *   - Then followed by \t, size obtained and \r\n (End of line char).
 TODO: CHECK FOR INPUT ERRORS, TEST, SEE REQUIREMENTS.
 */
+
 int main(int argc, char *argv[]){
   int result = 0;
-  char buffer[PATH_MAX];
-  DIR *mydir = opendir(getcwd(buffer, sizeof(buffer)));
-  struct dirent *myfile;
-  while((myfile = readdir(mydir)) != NULL){
-    // Open the file using open
-    int fd = open(myfile->d_name, O_RDWR, 0666);
-    if (myfile->d_type == DT_REG){
-      // Move the pointer to the end counting its size
-      int fileSize = lseek(fd,0,SEEK_END);
-      // close it (the file, not the dir)
-      close(fd);
-      // Print the data.
-      printf("%s\t%i\r\n", myfile->d_name, fileSize);
+  if (argc > 1){
+    printf("mysize: Error, argument introduced. errno: %i\n",strerror(errno));
+    result = -1;
+  } else {
+    char buffer[PATH_MAX];
+    DIR *mydir = opendir(getcwd(buffer, sizeof(buffer)));
+    struct dirent *myfile;
+    while((myfile = readdir(mydir)) != NULL){
+      // Open the file using open
+      int fd = open(myfile->d_name, O_RDWR, 0666);
+      if (myfile->d_type == DT_REG){
+        // Move the pointer to the end counting its size
+        int fileSize = lseek(fd,0,SEEK_END);
+        // close it (the file, not the dir)
+        close(fd);
+        // Print the data.
+        printf("%s\t%i\r\n", myfile->d_name, fileSize);
+      }
     }
+    // Close the directory.
+    closedir(mydir);
   }
-  // Close the directory.
-  closedir(mydir);
   return result;
 }
