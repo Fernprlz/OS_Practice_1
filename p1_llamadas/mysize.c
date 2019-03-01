abfaaa0e45165eca96c858e127a77956164a1e75
Fernpelz <fern.perloz@gmail.com>
2019-03-01 23:52:49 +0100

    mysize partially implemented

--- p1_llamadas/mysize.c
+++ p1_llamadas/mysize.c
@@ -5,8 +5,35 @@
 #include <dirent.h>
 #include <unistd.h>
 
-
-int main(int argc, char *argv[])
-{
-
+/*
+* - Obtain the current directory with getcwd();
+* - Read the entries of the directory using readdir
+* - IF ENTRY IS A REGULAR FIL (d_type from dirent == DT_REG)
+*   - Open the file using Open
+*   - Move the pointer to the end of the file and obtain value with lseek
+*   - Close file with Close
+*   - Print the name of the file (d_name of the structure)
+*   - Then followed by \t, size obtained and \r\n (End of line char).
+TODO: CHECK FOR INPUT ERRORS, TEST, SEE REQUIREMENTS.
+*/
+int main(int argc, char *argv[]){
+  int result = 0;
+  char buffer[PATH_MAX];
+  DIR *mydir = opendir(getcwd(buffer, sizeof(buffer)));
+  struct dirent *myfile;
+  while((myfile = readdir(mydir)) != NULL){
+    // Open the file using open
+    int fd = open(myfile->d_name, O_RDWR, 0666);
+    if (myfile->d_type == DT_REG){
+      // Move the pointer to the end counting its size
+      int fileSize = lseek(fd,0,SEEK_END);
+      // close it (the file, not the dir)
+      close(fd);
+      // Print the data.
+      printf("%s\t%i\r\n", myfile->d_name, fileSize);
+    }
+  }
+  // Close the directory.
+  closedir(mydir);
+  return result;
 }
